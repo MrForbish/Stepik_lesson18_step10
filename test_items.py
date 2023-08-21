228a89a7a319799bf4b91c8a98f63932e23f0d57
MrForbish <stalingroup5@gmail.com>
2023-08-21 17:47:25 +0300

    fix function

--- test_items.py
+++ test_items.py
@@ -1,18 +1,16 @@
 import pytest
 import time
-from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 
 link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 
 def test_exists_button_to_basket_for_any_languages(browser):
-    try:
-        browser.get(link)
-        curr_language = browser.execute_script('return window.navigator.language || window.navigator.userLanguage')
-        if 'fr' in curr_language:
-            time.sleep(30)
-        browser.find_element(By.CSS_SELECTOR, '#add_to_basket_form .btn-add-to-basket')
-    except NoSuchElementException:
-        return False
-    return True
+    browser.get(link)
+    curr_language = browser.execute_script('return window.navigator.language || window.navigator.userLanguage')
+    if 'fr' in curr_language:
+        time.sleep(30)
+    button_basket = browser.find_elements(By.CSS_SELECTOR, '#add_to_basket_form .btn-add-to-basket')
+    print("Count ", len(button_basket))
+    assert len(button_basket) > 0, 'Не найдена кнопка добавления в корзину'
+    assert len(button_basket) < 2, 'Кнопка добавления в корзину не уникальна'
