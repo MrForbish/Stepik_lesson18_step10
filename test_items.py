1a289af00660164d7c679a2e8a3d70fc8da382fe
MrForbish <stalingroup5@gmail.com>
2023-08-21 17:08:56 +0300

    lesson18_step10

---
+++ test_items.py
@@ -0,0 +1,18 @@
+import pytest
+import time
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
+
+def test_lang(browser):
+    try:
+        browser.get(link)
+        curr_language = browser.execute_script('return window.navigator.language || window.navigator.userLanguage')
+        if 'fr' in curr_language:
+            time.sleep(30)
+        browser.find_element(By.CSS_SELECTOR, '#add_to_basket_form .btn-add-to-basket')
+    except NoSuchElementException:
+        return False
+    return True
